0563093f20d23b5f1885362aeebb93e3364e11bf
shipali k <preethikotyan17@gmail.com>
2025-06-21 20:13:01 +0530

    Fix syntax error in reviews/urls.py

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path  
 from . import views
 from django.contrib.auth import views as auth_views
-from reviews.views import run_setup
+
 
 urlpatterns = [
     path('review/', views.submit_review, name='submit_review'),
